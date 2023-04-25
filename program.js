c0cd085a15fb730211ac1c76c80dccf058dfd473
MyoMin <MayoMinPhai@gmail.com>
2023-04-25 10:23:13 -0400

    initial commit

---
+++ program.js
@@ -0,0 +1,11 @@
+// var firstName;
+// firstName = "Myo";
+// lastName = "Phai";
+// console.log(" Hello, my name is: " + firstName + " " + lastName);
+
+var customerId = 555;
+var customerFirstName = "Myo";
+var customerLastName = "Phai";
+console.log(customerId);
+console.log(customerFirstName);
+console.log(customerLastName);
