cfd151b5191652c09e0d44caa993f9bfadf7946e
MyoMin <MayoMinPhai@gmail.com>
2023-04-28 10:03:55 -0400

    random commits

---
+++ hello-world-website/better_website/scripts/greetings.js
@@ -0,0 +1,8 @@
+"use strict"
+
+function showGreeting(){
+const messageDiv = document.getElementById("myMessage");
+    //console.log(messageDiv);
+messageDiv.innerHTML = "Hello there!";}
+
+showGreeting(myMessage);
